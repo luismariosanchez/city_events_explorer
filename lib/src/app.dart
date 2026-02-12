b2806017f3edf59405269cbda3ebd71245423ff4
Luis Mario <116817013+luismariosanchez@users.noreply.github.com>
2026-02-12 13:03:47 +0100

    chore: add core providers with Riverpod

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,20 @@
+import 'package:city_events_explorer/src/presentation/providers/repositories_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final sharedPreferences = await SharedPreferences.getInstance();
+  runApp(
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/src/presentation/providers/repositories_provider.dart
@@ -0,0 +1,37 @@
+import 'package:city_events_explorer/src/data/repositories/event_repository_impl.dart';
+import 'package:city_events_explorer/src/data/repositories/favorites_repository_impl.dart';
+import 'package:city_events_explorer/src/domain/usecases/get_events_usecase.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:city_events_explorer/src/data/datasources/local_event_datasource.dart';
+import 'package:city_events_explorer/src/domain/repositories/event_repository.dart';
+import 'package:city_events_explorer/src/domain/repositories/favorite_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final Provider<SharedPreferences>
+sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+  throw UnimplementedError(
+    'sharedPreferencesProvider must be provided with a concrete implementation.',
+  );
+});
+
+final Provider<LocalEventDataSource> localEventDataSourceProvider =
+    Provider<LocalEventDataSource>((Ref ref) {
+      return LocalEventDataSource();
+    });
+
+final Provider<EventRepository> eventRepositoryProvider =
+    Provider<EventRepository>((Ref ref) {
+      final LocalEventDataSource dataSource = ref.read(localEventDataSourceProvider);
+      return EventRepositoryImpl(dataSource: dataSource);
+    });
+
+final Provider<FavoritesRepository> favoritesRepositoryProvider =
+    Provider<FavoritesRepository>((ref) {
+      final prefs = ref.read(sharedPreferencesProvider);
+      return FavoritesRepositoryImpl(sharedPreferences: prefs);
+    });
+
+final getEventsUseCaseProvider = Provider<GetEventsUseCase>((ref) {
+  final repo = ref.watch(eventRepositoryProvider);
+  return GetEventsUseCase(repo);
+});
