4be8922515cceae5e90cbfb778213de387942da3
Luis Mario <116817013+luismariosanchez@users.noreply.github.com>
2026-02-12 10:55:25 +0100

    feat(domain): define EventRepository contract

---
+++ lib/src/domain/repositories/event_repository.dart
@@ -0,0 +1,7 @@
+import 'package:city_events_explorer/src/domain/entities/event.dart';
+
+abstract class EventRepository {
+  Future<List<Event>> getEvents();
+
+  Future<List<Event>> getEventsFiltered({String? category, DateTime? startDate, DateTime? endDate});
+}
