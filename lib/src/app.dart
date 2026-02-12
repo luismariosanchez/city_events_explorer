cc1157885085bccffed7f443f2a25cbdd7c2bed8
Luis Mario <116817013+luismariosanchez@users.noreply.github.com>
2026-02-12 14:30:04 +0100

    feat(l10n): configure Flutter localization

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,11 @@
 import 'package:city_events_explorer/src/presentation/providers/repositories_provider.dart';
 import 'package:city_events_explorer/src/presentation/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../l10n/app_localizations.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +25,22 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(routerConfig: appRouter);
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('en')],
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFF111113),
+        brightness: Brightness.dark,
+        progressIndicatorTheme: const ProgressIndicatorThemeData(
+          circularTrackColor: Colors.white,
+        ),
+      ),
+    );
   }
 }
