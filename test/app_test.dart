41dc67865e7f72bdc06c9b9f853712f679909595
Luis Mario <116817013+luismariosanchez@users.noreply.github.com>
2026-02-12 23:03:26 +0100

    chore: remove dummy test

--- test/app_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  group('App bootstrap', () {
-    test('dummy test to ensure test runner works', () {
-      expect(true, true);
-    });
-  });
-}
